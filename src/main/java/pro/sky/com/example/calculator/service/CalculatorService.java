2a4fd2f9662b201addf0eedeeaa9df54596d2728
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-05-15 22:31:58 +0800

    Копипаст со старого проекта

---
+++ src/main/java/pro/sky/com/example/calculator/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package pro.sky.com.example.calculator.service;
+
+public interface CalculatorService {
+    int sum(int num1, int num2);
+
+    int subtract(int num1, int num2);
+
+    int multiply(int num1, int num2);
+
+    int divide(int num1, int num2);
+}
